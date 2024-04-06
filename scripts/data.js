8a4b3141279ab4572a3e7f0b8e743fc3c4dabcc3
Kyle Jussab <kylejussab@gmail.com>
2024-04-06 00:03:00 -0700

    Star, Platform, Publisher Filters

--- scripts/data.js
+++ scripts/data.js
@@ -1,30 +1,47 @@
 let gamesData = [
     {
+        image: "assets/box art/haloInfinite.webp",
+        title: "Halo Infinite",
+        publisher: "assets/xboxGameStudios.jpeg",
+        publisherText: "Microsoft Xbox Game Studios",
+        platform: ["assets/xboxSeriesXS.png", "assets/xboxOne.png", "assets/windows.png"],
+        platformText: "XboxSeriesX/S, XboxOne, Windows",
+        developer: "assets/343Industries.png",
+        developerText: "343 Industries",
+        genre: "First-person shooter",
+        price: "$59.99",
+        stars: 2.5,
+        rating: "87%",
+        description: "Something"
+    },
+    {
         image: "https://pbs.twimg.com/media/C2FlHhUXEAINy6w.jpg",
         title: "The Legend of Zelda: Breath of the Wild",
         publisher: "assets/nintendo.png",
         publisherText: "Nintendo",
         platform: ["assets/switch.webp"],
-        platformText: ["Switch"],
+        platformText: "Switch",
         developer: "assets/nintendoEPD.png",
         developerText: "Nintendo EPD",
         genre: "Open world",
         price: "$59.99",
-        rating: "4/5",
+        stars: 5,
+        rating: "97%",
         description: "Something"        
     },
     {
         image: "https://howlongtobeat.com/games/41753_The_Last_of_Us_Part_II.jpg",
         title: "The Last of Us Part II",
-        publisher: "assets/sony.png",
-        publisherText: "Sony",
+        publisher: "assets/sonyInteractive.png",
+        publisherText: "Sony Interactive Entertainment",
         platform: ["assets/ps4.png", "assets/ps5.png"],
-        platformText: ["Playstation 4", "Playstation 5"],
+        platformText: "Playstation4, Playstation5",
         developer: "assets/naughtyDog.png",
         developerText: "Naughty Dog",
         genre: "Survival",
         price: "$59.99",
-        rating: "4/5",        
+        stars: 4.5,
+        rating: "93%",        
         description: "Something"
     }
 ];
--- scripts/main.js
+++ scripts/main.js
@@ -10,31 +10,100 @@ addImageElement = (div, data, className) => {
     div.appendChild(tempImage);
 };
 
-posts = gamesData.map(post => {
-    const card = cardTemplate.content.cloneNode(true).children[0];
+addStarRating = (div, stars, className) => {
+    let hasHalfRating;
+    let numberOfStars = Math.floor(stars);
 
-    const title = card.querySelector("[data-title]");
-    const details = card.querySelector("[data-details]");
-    const genre = card.querySelector("[data-genre]");
-    const price = card.querySelector("[data-price]");
-    const image = card.querySelector("[data-img]");
-    const description = card.querySelector("[data-description]");
+    //If there's half a star rating
+    if(stars % 1 != 0){
+        hasHalfRating = true;
+    }
+    else{
+        hasHalfRating = false;
+    }
 
-    title.textContent = post.title;
-    addImageElement(details, post.publisher, "card-publisher");
+    for(let i = 1; i < 6; i++){
+        const starImage = document.createElement('img');
+        if(i <= numberOfStars){
+            starImage.src = "assets/fullStar.png";
+        }
+        else if(hasHalfRating){
+            starImage.src = "assets/halfStar.png";
+            hasHalfRating = false;
+        }
+        else{
+            starImage.src = "assets/noStar.png";
+        }
+        starImage.className = className;
+        div.appendChild(starImage);
+    }
+}
 
-    //As there can be multiple platforms, loop over all platform images
-    post.platform.forEach(platformImage => {
-        addImageElement(details, platformImage, "card-platform");
+displayFilteredGames = (filteredGames) => {
+    gameCardContainer.innerHTML = '';
+
+    // Add data to cards in HTML
+    posts = filteredGames.map(post => {
+        const card = cardTemplate.content.cloneNode(true).children[0];
+
+        const title = card.querySelector("[data-title]");
+        const details = card.querySelector("[data-details]");
+        const stars = card.querySelector("[data-stars]");
+        const rating = card.querySelector("[data-rating]");
+        const genre = card.querySelector("[data-genre]");
+        const price = card.querySelector("[data-price]");
+        const image = card.querySelector("[data-img]");
+        const description = card.querySelector("[data-description]");
+
+        title.textContent = post.title;
+        addImageElement(details, post.publisher, "card-publisher");
+
+        //As there can be multiple platforms, loop over all platform images
+        post.platform.forEach(platformImage => {
+            addImageElement(details, platformImage, "card-platform");
+        });
+
+        addImageElement(details, post.developer, "card-developer");
+        addStarRating(stars, post.stars, "card-star");
+        rating.textContent = "Rating: " + post.rating;
+        genre.textContent = post.genre;
+        price.textContent = post.price;
+        addImageElement(image, post.image, "card-image");
+        description.textContent = post.description;
+
+        gameCardContainer.append(card);
+    });
+}
+
+applyFilters = () =>{
+    const selectedPublishers = Array.from(document.querySelectorAll('input[type=checkbox][id^=publisher-]:checked')).map(checkbox => checkbox.id.replace('publisher-', ''));
+    const selectedPlatforms = Array.from(document.querySelectorAll('input[type=checkbox][id^=platform-]:checked')).map(checkbox => checkbox.id.replace('platform-', ''));
+    const selectedStars = Array.from(document.querySelectorAll('input[type=checkbox][id^=stars-]:checked')).map(checkbox => parseFloat(checkbox.id.replace('stars-', '')));
+
+    const filteredGames = gamesData.filter((game) => {
+        const matchesPublishers = !selectedPublishers.length || selectedPublishers.some(publisher => game.publisherText.toLowerCase().includes(publisher));
+        
+        const matchesPlatforms = !selectedPlatforms.length || selectedPlatforms.some(platform => game.platformText.toLowerCase().includes(platform));
+
+        const matchesStars = !selectedStars.length || selectedStars.some(selectedStar => {
+            if(selectedStar % 1 === 0) {
+                return Math.floor(game.stars) === selectedStar || Math.floor(game.stars) + 0.5 === selectedStar;
+            } 
+            else {
+                return Math.floor(game.stars) + 0.5 === selectedStar || Math.floor(game.stars) === selectedStar;
+            }
+        });
+
+        return matchesPublishers && matchesPlatforms && matchesStars;
     });
 
-    addImageElement(details, post.developer, "card-developer");
+    displayFilteredGames(filteredGames);
+}
 
-    genre.textContent = post.genre;
-    price.innerHTML = post.price;
-    addImageElement(image, post.image, "card-image");
-    description.textContent = post.description;
-
-    gameCardContainer.append(card);
+//Add all event listeners
+document.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+    checkbox.addEventListener('change', applyFilters);
 });
 
+//Call applyFilters for the first load
+applyFilters();
